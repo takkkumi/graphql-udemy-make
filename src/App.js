2771d1c05b4a0077ad511862bc6ae61c4483f6b3
root <root@DESKTOP-AUVOD0O.localdomain>
2019-03-12 21:00:18 +0900

    file resqued

--- src/App.js
+++ src/App.js
@@ -1,14 +1,63 @@
 import React, { Component } from 'react';
+import {ApolloProvider} from 'react-apollo'
+import {searchRepositories} from './graphql' 
+import {Query} from 'react-apollo'
+import client from './client'
 
-
+const DEFAULT_STATE = {
+   "first": 5,
+  "last": null,
+  "before": null,
+  "after": null,
+  "query": "フロントエンドエンジニア"
+}
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = DEFAULT_STATE
+    this.handleChange=this.handleChange.bind(this)
+    this.handleSubmit=this.handleSubmit.bind(this)
+  }
+  handleChange(event){
+    this.setState({
+      ...DEFAULT_STATE,
+      query: event.target.value
+    })
+  }
+  handleSubmit(event){
+    event.preventDefault()
+  }
   render() {
+    const {query,first,last,before,after} = this.state
+    console.log({query})
+    
     return (
-      <div>
-        Hello graphQL
-      </div>
-    );
+      <ApolloProvider client={client}>
+      <form onSubmit={this.handleSubmit}>
+      <input value={query} onChange={this.handleChange}/> 
+      </form>
+      <Query
+      query={searchRepositories}
+      variables= {{query,first,last,before,after}}>
+        
+        {
+          ({loading, error, data}) =>{
+            if (loading) return 'Loading...'
+            if (error) return `Error! ${error.message}`
+            console.log(data.search)
+            const search = data.search
+            const repositoryCount = search.repositoryCount
+            const repositoryUnit = repositoryCount === 1?  "Repository" : "Repositories"
+            const title = `Github Repositories Search Results -${data.search.repositoryCount} ${repositoryUnit}`
+            return <h2>{title}</h2>
+          }
+        }
+     </Query >
+        
+      
+      </ApolloProvider >
+    )
   }
 }
 
